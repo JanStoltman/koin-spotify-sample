b8c382cacff096e0420914efec52638c4311e37f
Jan <stoltmanjan@gmail.com>
2018-09-05 22:46:26 +0200

    Gradle dependencies added

--- app/src/main/java/com/yggdralisk/koinspotifysample/LauncherActivity.kt
+++ app/src/main/java/com/yggdralisk/koinspotifysample/LauncherActivity.kt
@@ -3,10 +3,9 @@ package com.yggdralisk.koinspotifysample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
